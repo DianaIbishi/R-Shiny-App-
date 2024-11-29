c45808d267b50540d3161c1ae62b3f00878f3eea
Diana Ibishi <diana.ibishi@stud.unilu.ch>
2024-11-29 09:41:04 +0100

    Delete the header.

--- Shiny_project.R
+++ Shiny_project.R
@@ -1,11 +1,5 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
+
+
 # For this R-Project I aim building an interactive map with the shiny package.
 
 
